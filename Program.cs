a0d86c1f48108ae8c6f34839d62970d7324640fb
acallahan97 <callahanamanda877@gmail.com>
2021-12-01 08:41:01 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Evermore should win album of the year!");
+        }
+    }
+}
